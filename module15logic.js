f7a8d9e2f5f00d6eb63b1eb84920c098442debf3
Julian Adrien-Stark <119699025+julesstark22@users.noreply.github.com>
2023-05-22 16:58:03 -0400

    Update module15logic.js

--- module15logic.js
+++ module15logic.js
@@ -1,86 +1,55 @@
-import json
-import folium
-import requests
+// load the data
+d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson')
+  .then(data => {
+    // create the map
+    const map = L.map('map').setView([37.8, -96], 4);
 
-# Get earthquake data
-url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
-response = requests.get(url)
-data = response.json()
+    // add a tile layer to the map
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+      maxZoom: 18
+    }).addTo(map);
 
-earthquakes = data['features']
-magnitudes = []
-longitudes = []
-latitudes = []
+    // define a function to get the color based on the depth of the earthquake
+    const getColor = depth => {
+      if (depth > 90) return '#FF0000';
+      if (depth > 70) return '#FF6600';
+      if (depth > 50) return '#FFCC00';
+      if (depth > 30) return '#FFFF00';
+      if (depth > 10) return '#CCFF00';
+      return '#00FF00';
+    };
 
-for earthquake in earthquakes:
-    magnitude = earthquake['properties']['mag']
-    longitude = earthquake['geometry']['coordinates'][0]
-    latitude = earthquake['geometry']['coordinates'][1]
-    magnitudes.append(magnitude)
-    longitudes.append(longitude)
-    latitudes.append(latitude)
+    // define a function to get the radius based on the magnitude of the earthquake
+    const getRadius = magnitude => magnitude * 4;
 
-# Create dictionary to hold the data
-output_data = {
-    'magnitudes': magnitudes,
-    'longitudes': longitudes,
-    'latitudes': latitudes
-}
+    // add a circle marker for each earthquake
+    data.features.forEach(feature => {
+      const coordinates = feature.geometry.coordinates;
+      const magnitude = feature.properties.mag;
+      const depth = coordinates[2];
+      L.circleMarker([coordinates[1], coordinates[0]], {
+        color: getColor(depth),
+        fillColor: getColor(depth),
+        fillOpacity: 0.8,
+        radius: getRadius(magnitude)
+      })
+        .bindPopup(`<h3>${feature.properties.place}</h3><hr><p>Magnitude: ${magnitude}</p><p>Depth: ${depth} km</p>`)
+        .addTo(map);
+    });
 
-# Save the data to a JSON file
-with open('earthquake_data.json', 'w') as json_file:
-    json.dump(output_data, json_file)
-
-# Get earthquake data
-url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
-response = requests.get(url)
-data = response.json()
-earthquakes = data['features']
-
-# Create a map centered at (0, 0)
-map_center = [0, 0]
-zoom_level = 2
-map_osm = folium.Map(location=map_center, zoom_start=zoom_level)
-
-# Define marker colors based on depth
-def get_marker_color(depth):
-    if depth < 10:
-        return 'green'
-    elif depth < 50:
-        return 'orange'
-    else:
-        return 'red'
-
-# Add markers for each earthquake
-for earthquake in earthquakes:
-    magnitude = earthquake['properties']['mag']
-    depth = earthquake['geometry']['coordinates'][2]
-    latitude = earthquake['geometry']['coordinates'][1]
-    longitude = earthquake['geometry']['coordinates'][0]
-    marker_color = get_marker_color(depth)
-    
-    popup_text = f"Magnitude: {magnitude}<br>Depth: {depth} km"
-    marker = folium.CircleMarker(
-        location=[latitude, longitude],
-        radius=magnitude * 3,
-        color=marker_color,
-        fill=True,
-        fill_color=marker_color,
-        fill_opacity=0.7,
-        popup=popup_text
-    )
-    marker.add_to(map_osm)
-
-# Add legend
-legend_html = '''
-    <div style="position: fixed; bottom: 50px; left: 50px; z-index:9999; font-size: 14px;">
-    <p><strong>Legend:</strong></p>
-    <p><span style="background:green; padding: 3px 6px;"></span> Depth < 10 km</p>
-    <p><span style="background:orange; padding: 3px 6px;"></span> Depth 10-50 km</p>
-    <p><span style="background:red; padding: 3px 6px;"></span> Depth > 50 km</p>
-    </div>
-    '''
-map_osm.get_root().html.add_child(folium.Element(legend_html))
-
-# Save the map to an HTML file
-map_osm.save('earthquake_map.html')
+    // add a legend to the map
+    const legend = L.control({ position: 'bottomright' });
+    legend.onAdd = () => {
+      const div = L.DomUtil.create('div', 'info legend');
+      const grades = [-10, 10, 30, 50, 70, 90];
+      let labels = [];
+      for (let i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+          '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+          grades[i] + (grades[i + 1] ? '–' + grades[i + 1] + '<br>' : '+');
+      }
+      return div;
+    };
+    legend.addTo(map);
+  });
